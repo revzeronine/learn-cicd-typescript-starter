753960b8dac9caa01890112f990e4674e35f6a5a
revzeronine <274330765+revzeronine@users.noreply.github.com>
2026-05-12 10:22:38 -0400

    Add automatic code formatting using prettier

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -3,38 +3,36 @@ import { getAPIKey } from "../api/auth.js";
 import { describe, expect, test } from "vitest";
 
 describe("API Key", () => {
-
-
   test("Get key from 'authorization' header", () => {
     const header: IncomingHttpHeaders = {
-      "authorization": "ApiKey 12345678"
+      authorization: "ApiKey 12345678",
     };
     expect(getAPIKey(header)).toBe("12345678");
-  })
+  });
 
   test("Get null when missing ApiKey prefix", () => {
     const header: IncomingHttpHeaders = {
-      "authorization": "1234567"
-    }
+      authorization: "1234567",
+    };
     expect(getAPIKey(header)).toBe(null);
-  })
+  });
 
   test("Get null when missing apikey value", () => {
     const header: IncomingHttpHeaders = {
-      "authorization": "ApiKey"
-    }
+      authorization: "ApiKey",
+    };
     expect(getAPIKey(header)).toBe(null);
-  })
+  });
 
   test("Get null when no authorization header is present", () => {
-    const header: IncomingHttpHeaders = {}
+    const header: IncomingHttpHeaders = {};
     expect(getAPIKey(header)).toBe(null);
-  })
+  });
 
   test("Get null when authorization header is malformed", () => {
     const header: IncomingHttpHeaders = {
-      "authorization": "SomeApiKey 123445"
-    }
+      authorization: "SomeApiKey 123445",
+    };
     expect(getAPIKey(header)).toBe(null);
-  })
+  });
 });
